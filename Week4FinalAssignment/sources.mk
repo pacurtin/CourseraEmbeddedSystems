269c97b579db25f60e672a7c8108aaf234910e94
paddy <pacurtin@tcd.ie>
2017-10-27 15:18:07 +1000

    Simplifying code and altering makefile. Having strange problem where make clean is corrupting makefile

--- Week4FinalAssignment/sources.mk
+++ Week4FinalAssignment/sources.mk
@@ -2,18 +2,20 @@
 # Copyright (C) 2017 by Alex Fosdick - University of Colorado
 #
 # Redistribution, modification or use of this software in source or binary
-# forms is permitted as long as the files maintain this copyright. Users are 
+# forms is permitted as long as the files maintain this copyright. Users are
 # permitted to modify this and use it to learn about the field of embedded
 # software. Alex Fosdick and the University of Colorado are not liable for any
-# misuse of this material. 
+# misuse of this material.
 #
 #*****************************************************************************
 
-HOSTSOURCES = src/main.c \
-	src/memory.c \
-	src/course1.c \
-	src/data.c \
-	src/stats.c\
+HOSTSOURCES = src/main.c
+
+#\
+#	src/memory.c \
+#	src/course1.c \
+#	src/data.c \
+#	src/stats.c\
 
 MSP432SOURCES = src/main.c \
 	src/memory.c \
--- Week4FinalAssignment/src/main.c
+++ Week4FinalAssignment/src/main.c
@@ -10,45 +10,26 @@
  *****************************************************************************/
 /**
  * @file main.c
- * @brief Main entry point to the C1M2 Assessment
- *
- * This file contains the main code for the C1M2 assesment. Students
- * are not to change any of the code, they are instead supposed to compile
- * these files with their makefile.
+ * @brief Main entry point to the C1M4 Assessment
  *
  * @author Alex Fosdick
  * @date April 1 2017
  *
  */
-#include "platform.h"
-#include "memory.h"
+//#include "course1.h"
+#include <stdio.h>
 
-#define MAX_LENGTH (10)
-char buffer[MAX_LENGTH];
 
-/* A pretty boring main file */
+/* Simple one line main. To run tests use -DCOURSE1 compile time switch */
 int main(void) {
-  unsigned int i;
-  char value;
 
-  /* Code below does some arbitrary memory Reads & writes */
-  clear_all(buffer, MAX_LENGTH);
-  set_all( ( buffer + 8 ), 43, 2); 
-  set_value(buffer, 0, 0x61);
-  value = get_value(buffer, 9);
-  set_value(buffer, 9, (value + 0x27));
-  set_value(buffer, 3, 0x37);
-  set_value(buffer, 1, 88);
-  set_value(buffer, 4, '2');
-  value = get_value(buffer, 1);
-  set_value(buffer, 2, 121);
-  set_value(buffer, 7, (value - 12));
-  set_value(buffer, 5, 0x5F);
+  #ifdef COURSE1
+    //course1();
+    printf("Hello from course 1");
+  #endif
 
-  for ( i = 0; i < MAX_LENGTH; i++ ){
-    PRINTF("%c", buffer[i]);
-  }
-  PRINTF("\n");
+	printf("Main ran. Did it say hello?");
+
   return 0;
 }
 
